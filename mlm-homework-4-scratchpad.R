48a39184a4a00be7e823c08cb65b4dc2f722d128
Langdon Holmes <langdon.holmes@vanderbilt.edu>
2023-04-18 18:24:11 +0000

    before rebase

---
+++ mlm-homework-4-scratchpad.R
@@ -0,0 +1,46 @@
+10.460389 / (sqrt(5.042452*46.392981))
+
+94.673772 / sqrt(55.704286*393.529735)
+
+-.080131 / sqrt(41.842717*23.712014)
+
+# intercepts
+7.719267 / sqrt(4.303871*41.842717)
+
+# slopes
+5.305848 / sqrt(1.769467*23.712014)
+
+# Math intercept and slope
+.824656 / sqrt(4.303871*1.769467)
+
+# English intercept and slope
+-.080131 / sqrt(41.842717*23.712014)
+
+chi_square <- 17231.024-17197.321
+pchisq(chi_square, 1, lower.tail = FALSE)
+10011.414395-7795.427551
+
+pt(-41.602, df = 76.193)
+
+43.83 + 32.52 + 7.58
+
+43.83 / 83.93
+
+32.52 / 83.93
+
+7.58 / 83.93
+
+var.decomp <- function(var1, var2, var3){
+  overall = var1+var2+var3
+  ICC1 = var1 / overall
+  ICC2 = var2 / overall
+  ICC3 = var3 / overall
+  print(overall)
+  cat(ICC1, ICC2, ICC3)
+}
+
+var.decomp(14.844, 11.729, 3.0135)
+
+var.decomp(54.542, 29.158, 11.486)
+
+var.decomp(19.225, 12.349, 3.3155)
